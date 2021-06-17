0a54b2e4c7a980d8798e623c95c824bbb48598c4
aleksejlinkov <a.linkov@me.com>
2021-06-17 04:04:40 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,18 +12,13 @@ let package = Package(
             targets: ["TSUtils"]),
     ],
     dependencies: [
-        .package(
-            name: "TSLog",
-            url: "https://github.com/linkov/ts-log.git",
-            .branch("master")
-        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TSUtils",
-            dependencies: ["TSLog"]),
+            dependencies: []),
         .testTarget(
             name: "TSUtilsTests",
             dependencies: ["TSUtils"]),
